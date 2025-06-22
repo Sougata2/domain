473c6dd1fd73ddb0e467b2b8e2525c83e2a475e4
sougata <sougata19582001@gmail.com>
2025-06-23 01:15:05 +0530

    handling Entity Not found exception.

--- src/main/java/com/sougata/domain/exceptions/DomainExceptionHandler.java
+++ src/main/java/com/sougata/domain/exceptions/DomainExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sougata.domain.exceptions;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +33,16 @@ public class DomainExceptionHandler {
 
     @ExceptionHandler(DefaultRoleNotFoundException.class)
     public ResponseEntity<ErrorDto> defaultRoleNotFoundException(DefaultRoleNotFoundException e, HttpServletRequest request) {
+        ErrorDto.ErrorDtoBuilder response = new ErrorDto.ErrorDtoBuilder();
+        response.message(e.getMessage());
+        response.path(request.getRequestURI());
+        response.status(HttpStatus.NOT_FOUND);
+        response.timestamp(new Timestamp(System.currentTimeMillis()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response.build());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorDto> entityNotFoundException(EntityNotFoundException e, HttpServletRequest request) {
         ErrorDto.ErrorDtoBuilder response = new ErrorDto.ErrorDtoBuilder();
         response.message(e.getMessage());
         response.path(request.getRequestURI());
