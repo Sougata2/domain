c15f700c35421fe67d18969565d8bac97ebf19ee
sougata <sougata19582001@gmail.com>
2025-08-16 14:49:51 +0530

    added content disposition exposed header for file download.

--- src/main/java/com/sougata/domain/auth/config/AuthConfig.java
+++ src/main/java/com/sougata/domain/auth/config/AuthConfig.java
@@ -3,6 +3,7 @@ package com.sougata.domain.auth.config;
 import com.sougata.domain.auth.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,6 +42,7 @@ public class AuthConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173/", "*"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setExposedHeaders(List.of(HttpHeaders.CONTENT_DISPOSITION));
         corsConfiguration.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
