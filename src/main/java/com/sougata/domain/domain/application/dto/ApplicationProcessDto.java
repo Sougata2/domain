939250dfbd571592efb0c4908c6ee41dedc83038
sougata <sougata19582001@gmail.com>
2025-08-26 01:58:44 +0530

    added application process dto to take action, application, assignee, assigner and movement for relaying application  to next stage.

---
+++ src/main/java/com/sougata/domain/domain/application/dto/ApplicationProcessDto.java
@@ -0,0 +1,19 @@
+package com.sougata.domain.domain.application.dto;
+
+import com.sougata.domain.domain.workFlowAction.dto.WorkFlowActionDto;
+import com.sougata.domain.domain.workFlowAction.enums.WorkFlowMovement;
+import com.sougata.domain.user.dto.UserDto;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationProcessDto {
+    private ApplicationDto application;
+    private WorkFlowActionDto workFlowAction;
+    private UserDto assignee;
+    private UserDto assigner;
+    private WorkFlowMovement movement;
+}
