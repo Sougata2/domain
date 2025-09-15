d601da4b91c57e681b3518786b02cafd994a9cda
sougata <sougatadas@cdac.in>
2025-09-15 18:03:10 +0530

    find the status by status name.

--- src/main/java/com/sougata/domain/domain/status/repository/StatusRepository.java
+++ src/main/java/com/sougata/domain/domain/status/repository/StatusRepository.java
@@ -2,8 +2,13 @@ package com.sougata.domain.domain.status.repository;
 
 import com.sougata.domain.domain.status.entity.StatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StatusRepository extends JpaRepository<StatusEntity, Long> {
+    @Query("select e from StatusEntity e where e.name = :statusName")
+    Optional<StatusEntity> findByStatusName(String statusName);
 }
