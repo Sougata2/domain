38ceff86c1a0db1d3a8c96a71fa399fcc1aab33b
sougata <sougata19582001@gmail.com>
2025-10-03 00:19:01 +0530

    added new WorkFlowActionActionType

--- src/main/java/com/sougata/domain/domain/status/enums/WorkFlowActionType.java
+++ src/main/java/com/sougata/domain/domain/status/enums/WorkFlowActionType.java
@@ -2,6 +2,7 @@ package com.sougata.domain.domain.status.enums;
 
 public enum WorkFlowActionType {
     ACTION_WITH_UPLOAD,
+    TEST_REPORT_VIEW,
     CREATE_JOB_CARD,
     TEST_CARD,
     PAYMENT,
